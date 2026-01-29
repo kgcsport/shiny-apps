baa60be65e10e4db9faf7d2b77872d6b4263c986
Kyle Coombs <kyle.g.coombs@gmail.com>
2026-01-29 18:39:13 -0500

    google sheet encryption

--- final_question_reveal/bcrypt.R
+++ final_question_reveal/bcrypt.R
@@ -1,18 +1,20 @@
 # one_time_make_credentials.R
 if (!requireNamespace("pacman", quietly = TRUE)) install.packages("pacman")
-pacman::p_load(tidyverse, bcrypt)
+pacman::p_load(tidyverse, bcrypt, googledrive, googlesheets4)
 
 # Step 1. Your plain-text roster (temporary)
-roster <- read_csv('final_question_reveal/roster.csv') %>%
+sheet_id='1k5mukmbiANheozOeW_NEfWr_GhT2Ly8flUKiMbIY5tg'
+
+googlesheets4::read_sheet(sheet_id, sheet = "users") %>%
     rowwise() %>%
     mutate(pw_hash=bcrypt::hashpw(password)) %>%
     select(-password) %>%
-    write_csv("final_question_reveal/credentials.csv")
+    googlesheets4::sheet_write(sheet_id, sheet = "credentials")
 
-txt <- readChar("final_question_reveal/credentials.csv", file.info("final_question_reveal/credentials.csv")$size, useBytes = TRUE)
-txt_b64 <- base64enc::base64encode(charToRaw(txt))
-# add to Renviron
-Sys.setenv(CRED_B64=txt_b64)
-cred_b64 <- Sys.getenv("CRED_B64")
-cred_txt <- rawToChar(base64enc::base64decode(cred_b64))
-cred_df <- read_csv(cred_txt)
+# txt <- readChar("final_question_reveal/credentials.csv", file.info("final_question_reveal/credentials.csv")$size, useBytes = TRUE)
+# txt_b64 <- base64enc::base64encode(charToRaw(txt))
+# # add to Renviron
+# Sys.setenv(CRED_B64=txt_b64)
+# cred_b64 <- Sys.getenv("CRED_B64")
+# cred_txt <- rawToChar(base64enc::base64decode(cred_b64))
+# cred_df <- read_csv(cred_txt)
